c49db171f2f7b941a760aa3a633e7ba760c5ef6a
Santosh kurmi <xantosh121@gmail.com>
2026-05-24 20:38:03 +0545

    fixed gogle sevice

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -43,10 +44,15 @@ android {
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            ndk {
+            debugSymbolLevel = "NONE"
+        }
         }
         getByName("debug") {
             applicationIdSuffix = ".debug"
